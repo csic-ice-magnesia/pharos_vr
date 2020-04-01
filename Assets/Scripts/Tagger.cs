b7bd2263d056f0202ccefe34c8540fbe1509b2e1
Albert Garcia <garciagarcia@ice.csic.es>
2020-04-01 17:43:06 +0200

    change logos and title

--- Assets/Scripts/Tagger.cs
+++ Assets/Scripts/Tagger.cs
@@ -5,41 +5,84 @@ using UnityEngine.UI;
 
 public class Tagger : MonoBehaviour
 {
+    // Camera to which the tags should always face to.
     public Transform mCameraToLookAt;
+    // Prefab to spawn tags.
     public GameObject mTagObjectPrefab;
+    // Distance at which the tag will be invisble [kpc].
+    public float mNearDistance = 16.0f;
+    // Distance at which the tag will be fully visible [kpc].
+    public float mFarDistance = 64.0f;
 
+    // Data struct to hold tag information.
     struct Tag
     {
         public string name;
         public Vector3 position;
     };
+    // List of tag data.
+    private List<Tag> mTags;
+    // List of tag instances.
+    private List<GameObject> mTagInstances;
 
-    List<Tag> mTags;
-    List<GameObject> mTagObjects;
-
-    // Start is called before the first frame update
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
     void Start()
     {
         mTags = new List<Tag>();
-        mTagObjects = new List<GameObject>();
+        mTagInstances = new List<GameObject>();
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame.
+    /// 
+    /// Make sure that all tags are facing the camera and update their alpha
+    /// value depending on the distance to the camera.
+    /// </summary>
     void Update()
     {
-        foreach (GameObject t in mTagObjects)
+        foreach (GameObject t in mTagInstances)
         {
-            t.transform.rotation = Camera.main.transform.rotation;
-            Vector3 objectNormal = t.transform.rotation * Vector3.forward;
-            Vector3 cameraToText = t.transform.position - Camera.main.transform.position;
-            float f = Vector3.Dot(objectNormal, cameraToText);
-            if (f < 0f)
+            // Update tag rotation so that it faces the camera.
             {
-                t.transform.Rotate(0f, 180f, 0f);
+                t.transform.rotation = Camera.main.transform.rotation;
+                Vector3 objectNormal = t.transform.rotation * 
+                                       Vector3.forward;
+                Vector3 cameraToText = t.transform.position -
+                                       Camera.main.transform.position;
+                float f = Vector3.Dot(objectNormal, cameraToText);
+                if (f < 0f)
+                {
+                    t.transform.Rotate(0f, 180f, 0f);
+                }
             }
+            // Update tag opacity according to its distance to camera.
+            /*{
+                float distanceToCamera = Vector3.Distance(
+                    t.GetComponent<RectTransform>().transform.position,
+                    mCameraToLookAt.transform.position
+                );
+
+                Debug.Log("Tag " + t.name);
+                Debug.Log("Position " + t.GetComponent<RectTransform>().transform.position);
+                Debug.Log("Distance " + distanceToCamera);
+
+
+                Color tagColor = Color.white;
+                tagColor.a = Mathf.Clamp(distanceToCamera / mNearDistance, 0.0f, 1.0f);
+
+                Debug.Log("Opacity " + tagColor.a);
+                t.GetComponent<Text>().material.color = tagColor;
+            }*/
         }
     }
 
+    /// <summary>
+    /// Add tag data to the list.
+    /// </summary>
+    /// <param name="name">Name of the pulsar.</param>
+    /// <param name="position">3D position in space [kpc, kpc, kpc].</param>
     public void AddTag(string name, Vector3 position)
     {
         Tag tag = new Tag
@@ -51,32 +94,42 @@ public class Tagger : MonoBehaviour
         mTags.Add(tag);
     }
 
-    public void AddTags(List<Vector3> positions, List<string> names)
+    /// <summary>
+    /// Add tags data to the list.
+    /// </summary>
+    /// <param name="names">Names of the pulsars.</param>
+    /// <param name="positions">3D positions in space [kpc, kpc, kpc].</param>
+    public void AddTags(List<string> names, List<Vector3> positions)
     {
         for (int i = 0; i < names.Count; ++i)
         {
-            Tag tag = new Tag
-            {
-                name = names[i],
-                position = positions[i]
-            };
-
-            mTags.Add(tag);
+            AddTag(names[i], positions[i]);
         }
     }
 
+    /// <summary>
+    /// Create and instantiate tags.
+    /// 
+    /// Instantiate each tag with its corresponding data.
+    /// </summary>
     public void CreateTags()
     {
         foreach (Tag t in mTags)
         {
-            var tagInstance = Instantiate(mTagObjectPrefab, t.position, Quaternion.identity);
+            var tagInstance = Instantiate(
+                mTagObjectPrefab,
+                t.position,
+                Quaternion.identity
+            );
 
             tagInstance.name = "Tag_" + t.name;
             tagInstance.transform.SetParent(this.transform);
             tagInstance.GetComponent<Text>().text = t.name;
+            tagInstance.GetComponent<Text>().alignByGeometry = true;
+            tagInstance.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
             tagInstance.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 
-            mTagObjects.Add(tagInstance);
+            mTagInstances.Add(tagInstance);
         }
     }
 }
